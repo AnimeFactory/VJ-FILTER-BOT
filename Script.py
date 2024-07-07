604ea553f9033ab27a84e9a6f178ab3e0e380224
AnimeFactory <166727007+AnimeFactory@users.noreply.github.com>
2024-07-07 18:16:12 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -15,8 +15,8 @@ Hᴇʀᴇ Is Tʜᴇ Hᴇʟᴘ Fᴏʀ Mʏ Cᴏᴍᴍᴀɴᴅs.</b>"""
     
 ‣ ᴍʏ ɴᴀᴍᴇ : <a href=https://t.me/{}>{}</a>
 ‣ ᴍʏ ʙᴇsᴛ ғʀɪᴇɴᴅ : <a href='tg://settings'>ᴛʜɪs ᴘᴇʀsᴏɴ</a> 
-𝙾𝚆𝙽𝙴𝚁 : <a href='https://t.me/Sayyed_kashifali'>𝚂𝚊𝚢𝚢𝚎𝚍 𝚔𝚊𝚜𝚑𝚒𝚏𝚊𝚕𝚒</a>
-‣ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/Nobita_MUI'>𝙹𝙰𝙲𝙺</a> 
+‣ OWNER : <a href='https://t.me/Sayyed_kashifali'>Sᴀʏʏᴇᴅ ᴋᴀsʜɪғᴀʟɪ</a>
+‣ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/Nobita_MUI'>NOBITA</a> 
 ‣ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>ᴘʏʀᴏɢʀᴀᴍ</a> 
 ‣ ʟᴀɴɢᴜᴀɢᴇ : <a href='https://www.python.org/download/releases/3.0/'>ᴘʏᴛʜᴏɴ 3</a> 
 ‣ ᴅᴀᴛᴀ ʙᴀsᴇ : <a href='https://www.mongodb.com/'>ᴍᴏɴɢᴏ ᴅʙ</a> 
